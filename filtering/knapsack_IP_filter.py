feb624b5c1ae25c6cb0c0c6e6e921314fde2fcfa
oisinbrady <oisinbrady@protonmail.com>
2022-02-22 18:49:21 +0000

    overlap, no-overlap, and storf size range added to optional IP constraints

--- filtering/knapsack_IP_filter.py
+++ filtering/knapsack_IP_filter.py
@@ -1,45 +1,15 @@
 from pulp import LpVariable, LpProblem, LpMaximize, value, LpAffineExpression, LpConstraint
 import re
 import argparse
+import itertools
 
 FILTER_ARGS = None
-
-
-def is_allowed_storf_range(storf: list) -> bool:
-    storf_len = len(re.findall('[AGTC]', storf[1]))
-    return FILTER_ARGS.storf_range[0] <= storf_len <= FILTER_ARGS.storf_range[1]
 
 
 def get_storf_delim(storf: list) -> list:
     colon_delimiters = [s.start() for s in re.finditer(r":",storf[0])] 
     pipe_delimiters = [s.start() for s in re.finditer(r"\|",storf[0])]
     return colon_delimiters, pipe_delimiters
-
-
-def is_allowed_storf_overlap(storf_index: int, group: list) -> bool:
-    # TODO
-    """
-    check if StORF satisfies overlap constraints
-    """
-    min_o = FILTER_ARGS.overlap_range[0]
-    max_o = FILTER_ARGS.overlap_range[1]
-    colon_delimiters, pipe_delimiters = get_storf_delim(uf_value_storfs[storf_index])
-    # the actual location of the StORF w.r.t the UR it is on
-    storf_act_loci = uf_value_storfs[storf_index][0][colon_delimiters[0]:pipe_delimiters[0]]
-    # get stop location of StORF
-    stop = int(storf_act_loci[(storf_act_loci.index('-')+1):])
-    # get start location of StORF
-    start = int(storf_act_loci[1:(storf_act_loci.index('-'))])
-    for i in range(storf_index + 1, len(group) - 1):
-        # get loci of current storf
-        colon_delimiters, pipe_delimiters = get_storf_delim(group[i])
-        uf_value_storfs[i][0][colon_delimiters[0]:pipe_delimiters[0]]
-        next_start = int(group[i][0][])
-
-        # if OVERLAP MIN <= stop - next start >= OVERLAP_MAX
-        if not min_o <= stop - next_start <= max_o:
-            return false
-
 
 
 def get_overlaps(uf_value_storfs: list) -> list:
@@ -57,29 +27,101 @@ def get_overlaps(uf_value_storfs: list) -> list:
         chromosome_rel_loci = (storf[0][colon_delimiters[1] + 1:pipe_delimiters[1]])
         # get _x at end of StORF location indicating its position in overlapping group
         overlap_num = int(chromosome_rel_loci[len(chromosome_rel_loci) - 1])
-        # select StORF dependent on its length constraints
-        selected = True
-        if FILTER_ARGS.storf_range:
-            selected = is_allowed_storf_range(storf)
-        if selected:
-            # iff StORF is the start of a new overlapping group
-            if overlap_num == 0:
-                overlapping_groups.append([])
-                group_i += 1
-            overlapping_groups[group_i].append(storf)
-
-
-    if FILTER_ARGS.overlap_range:
-        # remove any StORFs not within the overlap constraints
-        for i, group in enumerate(overlapping_groups):
-            for j, storf in enumerate(group):
-                if not is_allowed_storf_overlap(j, group):
-                    del overlapping_groups[i][j] 
-
-    
-    # print(overlapping_groups)
+        # iff StORF is the start of a new overlapping group
+        if overlap_num == 0:
+            overlapping_groups.append([])
+            group_i += 1
+        overlapping_groups[group_i].append(storf)
     return overlapping_groups
 
+
+def ip_set_storf_range(prob: LpProblem, storf_group: list, ip_vars: list, ip_var_id: int) -> None:
+    # for each StORF, constrain length to set range
+    storf_min = FILTER_ARGS.storf_range[0]
+    storf_max = FILTER_ARGS.storf_range[1]
+    for storf in storf_group:
+        storf_len = len(re.findall('[AGTC]', storf[1]))
+        prob += ip_vars[ip_var_id] * storf_len  <= storf_max
+        prob += ip_vars[ip_var_id] * storf_len >= storf_min
+        ip_var_id += 1
+        
+
+def ip_set_overlap_range(prob: LpProblem, storf_group: list, ip_vars: list, ip_var_id: int, group_id: int) -> None:
+    overlap_min = FILTER_ARGS.overlap_range[0]
+    overlap_max = FILTER_ARGS.overlap_range[1]
+    # get each StORF pair combination, c, from the group
+    s_pair_combination = [i for i in itertools.combinations(storf_group, 2)]  # combinations in order
+    s_pair_ids = [i for i in itertools.combinations([j for j in range(0, len(storf_group))], 2)] 
+    # for each c, get c[0].stop, c[1].start
+    for index, s_pair in enumerate(s_pair_combination):
+        # get stop location of StORF 1
+        s1_colon_delimiters, s1_pipe_delimiters = get_storf_delim(s_pair[0])
+        storf_1_loci = s_pair[0][0][s1_colon_delimiters[0] + 1:s1_pipe_delimiters[0]]
+        storf_1_stop = int(storf_1_loci[storf_1_loci.index('-') + 1:])
+
+        # get start location of StORF 2
+        s2_colon_delimiters, s2_pipe_delimiters = get_storf_delim(s_pair[1])
+        storf_2_loci = s_pair[1][0][s2_colon_delimiters[0] + 1:s2_pipe_delimiters[0]]
+        storf_2_start = int(storf_2_loci[:storf_2_loci.index('-')])
+        
+        # if overlap, apply constraints
+        if storf_1_stop - storf_2_start > 0:
+            id_1 = s_pair_ids[index][0]
+            id_2 = s_pair_ids[index][1]
+            # overlap must be within range
+            prob += (ip_vars[id_1] * storf_1_stop) - (ip_vars[id_2] * storf_2_start) <= overlap_max
+            prob += (ip_vars[id_1] * storf_1_stop) - (ip_vars[id_2] * storf_2_start) >= overlap_min
+            # all or none of each StORF in pair must be selected
+            prob += (ip_vars[id_1] * 1) + (ip_vars[id_2] * 1) <= 2
+        ip_var_id += 1
+
+
+def ip_set_storf_value(prob: LpProblem, storf_group: list, ip_vars: list) -> None:
+    # TODO
+    return
+
+
+def ip_set_no_overlaps(prob: LpProblem, storf_group: list, ip_vars: list, ip_var_id: int, group_id: int) -> None:
+    # constraint group
+    cg = [(i,1) for i in ip_vars[ip_var_id:ip_var_id + len(storf_group)]]
+    e = LpAffineExpression(cg)
+    # ensure only one StORF selected in group therefore RHS: <= 1
+    c = LpConstraint(e, -1, f"no_overlap_constraint(group {group_id})", 1)
+    prob += c
+    
+
+def ip_set_obj(prob: LpProblem, storf_groups: list, ip_vars: list) -> None:
+    ip_var_id = 0
+    obj_expression = []
+    for storf_group in storf_groups:
+        for storf in storf_group:
+            # storf id and storf value
+            obj_expression.append((ip_vars[ip_var_id], storf[2]))
+            ip_var_id += 1
+    # initialise objective function
+    prob += LpAffineExpression(obj_expression)
+
+
+def ip_set_constraints(prob: LpProblem, storf_groups: list, ip_vars: list) -> None:
+    ip_var_id = 0  # track the current IP variable (StORF) being used
+    # create constraints for each storf group
+    for group_id, storf_group in enumerate(storf_groups):
+        # TODO apply all run-time parameter constraints
+        
+        if FILTER_ARGS.storf_range:
+            ip_set_storf_range(prob, storf_group, ip_vars, ip_var_id)
+        
+        if FILTER_ARGS.overlap_range:
+            # TODO
+            ip_set_overlap_range(prob, storf_group, ip_vars, ip_var_id, group_id)  
+        else:
+            ip_set_no_overlaps(prob, storf_group, ip_vars, ip_var_id, group_id)
+
+        if FILTER_ARGS.stop_codon_type:
+            return  # TODO
+
+        ip_var_id += len(storf_group)
+    
 
 def filter(uf_value_storfs: list) -> list:
     '''
@@ -96,41 +138,22 @@ def filter(uf_value_storfs: list) -> list:
 
     return: a list of filtered StORFs
     '''
-    # convert to list for fast storf lookups later
+    # convert to list for fast lookup of selected storfs from IP solution
     storf_dict = {i: uf_value_storfs[i] for i in range(0, len(uf_value_storfs))}
     # initialise ip instance
     prob = LpProblem("StORF_Knapsack_Problem", LpMaximize)
-    # initialise ip variables
+    # initialise IP variables
     storf_ids = [f"x_{i}" for i in range(0, len(uf_value_storfs))]
     ip_vars = [LpVariable(storf_ids[i], lowBound=0, upBound=1, cat='Integer') for i in range(0, len(uf_value_storfs))]
     # get overlapping StORFs
     overlapping_storf_groups = get_overlaps(uf_value_storfs)
 
-    ip_var_id = 0
-    obj_expression = []
     # create objective function
-    for storf_group in overlapping_storf_groups:
-        for storf in storf_group:
-            # storf id and storf value
-            obj_expression.append((ip_vars[ip_var_id], storf[2]))
-            ip_var_id += 1
-    # initialise objective function
-    prob += LpAffineExpression(obj_expression)
+    ip_set_obj(prob, overlapping_storf_groups, ip_vars)
 
-    ip_var_id = 0
-    # for each group of overlapping storfs, create a constraint
-    for group_i, storf_group in enumerate(overlapping_storf_groups):
-        c_g = []  # constraint group
-        for storf_i, storf in enumerate(storf_group):
-            # storf id as a coefficient, e.g. selected storfs = 1*1
-            c_g.append((ip_vars[ip_var_id], 1))
-            ip_var_id += 1
-        # expression construction
-        e = LpAffineExpression(c_g)
-        # initialise constraint e = 1
-        c = LpConstraint(e, 0, f"StORF_group_constraint_{group_i}", 1)
-        # assign constraint to IP
-        prob += c
+    # create all constraints
+    ip_set_constraints(prob, overlapping_storf_groups, ip_vars)
+    
     # solve the IP model
     prob.solve()
 
@@ -164,7 +187,7 @@ def get_values(unfiltered_storfs: list):
     ls_len = len(largest_storf[1])  # largest storf length
     # define value of each StORF
     for storf in unfiltered_storfs:
-        value = 0
+        value = 1
 
         # TODO make this the default behaviour
         if FILTER_ARGS.storf_len:
@@ -174,8 +197,8 @@ def get_values(unfiltered_storfs: list):
         if FILTER_ARGS.simple_gc:
             # GC-content of each StORF
             value += len(re.findall('[GC]', storf[1])) / len(storf[1])
-        # TODO add all filtering option's logic
-        # GC content, overlap nt size, etc.
+        
+
         storf.append(value)
 
     
