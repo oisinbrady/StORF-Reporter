93e0457b0797d3055db0461f232a4962fb523f49
oisinbrady <oisinbrady@protonmail.com>
2022-02-24 17:46:19 +0000

    refactor + remove get_value func

--- filtering/knapsack_IP_filter.py
+++ filtering/knapsack_IP_filter.py
@@ -35,6 +35,38 @@ def get_overlaps(uf_value_storfs: list) -> list:
     return overlapping_groups
 
 
+def get_gc_average(storf_list: list) -> float: 
+    average_type = FILTER_ARGS.gc_profile[1]  # 0=mean, 1=median, 2=mode
+    if average_type == 0: # mean
+        total_gc = 0
+        num_storfs = len(storf_list)
+        for storf in storf_list:
+            total_gc += re.findall('[GC]', storf[1])
+        return total_gc/num_storfs
+    elif average_type == 1: # median
+        # order StORFs by length
+        ord_storfs = sorted(storf_list, key=lambda x: len(re.findall('[GC]', x[1])))
+        mid = (len(ord_storfs) - 1) // 2
+        if len(ord_storfs) % 2:  # odd
+            mid_gc_value = len(re.findall('[GC]', ord_storfs[mid][1]))
+            return mid_gc_value
+        else:  # even
+            # return interpolated median, average of both middle values
+            mid_1_gc = len(re.findall('[GC]', ord_storfs[mid][1]))
+            mid_2_gc = len(re.findall('[GC]', ord_storfs[mid + 1][1]))
+            mid_gc_value = (mid_1_gc + mid_2_gc) / 2.0
+            return mid_gc_value
+    elif average_type == 2:  # mode
+        # get gc count of each StORF
+        s_gc_len = [len(re.findall('[GC]', s[1])) for s in storf_list]
+        # get the most occuring gc count
+        mode_largest = max(set(s_gc_len), key=s_gc_len.count)
+
+        # TODO currently only selects the largest mode if multiple exist
+        # add some functionality for determining which mode to take?
+        return mode_largest
+
+
 def ip_set_storf_range(prob: LpProblem, storf_group: list, ip_vars: list, ip_var_id: int) -> None:
     # for each StORF, constrain length to set range
     storf_min = FILTER_ARGS.storf_range[0]
@@ -89,17 +121,36 @@ def ip_set_obj(prob: LpProblem, storf_groups: list, ip_vars: list) -> None:
     obj_expression = []
     for storf_group in storf_groups:
         for storf in storf_group:
-            # storf id and storf value
-            obj_expression.append((ip_vars[ip_var_id], storf[2]))
+            # create StORF IP variable representation using StORF ID (variable) and value (coefficient)
+            obj_expression.append((ip_vars[ip_var_id], 1))
             ip_var_id += 1
     # initialise objective function
     prob += LpAffineExpression(obj_expression)
 
 
+def ip_set_constraints(prob: LpProblem, storf_groups: list, ip_vars: list) -> None:
+    # create constraints for each storf
+    ip_var_id = 0  # track the current IP variable (StORF) being used
+    
+    for group_id, storf_group in enumerate(storf_groups):
+        
+        if FILTER_ARGS.storf_range:
+            ip_set_storf_range(prob, storf_group, ip_vars, ip_var_id)
+        
+        if FILTER_ARGS.overlap_range:
+            ip_set_overlap_range(prob, storf_group, ip_vars, ip_var_id, group_id)  
+        else:
+            ip_set_no_overlaps(prob, storf_group, ip_vars, ip_var_id, group_id)
+
+        ip_var_id += len(storf_group)
+
+
 def non_ip_filter(storf_list) -> list:
     """
-    Currently includes filtering by stop codon type, and via average gc profile
+    Currently includes filtering by stop codon type, and average gc profile
     """
+    # TODO convert these to IP constraints
+
     if FILTER_ARGS.gc_profile:
         ip_var_id = 0
         gc_average = get_gc_average(storf_list)
@@ -122,44 +173,24 @@ def non_ip_filter(storf_list) -> list:
             for sct in FILTER_ARGS.stop_codon_type:
                 if sct == stop_codon_1 or sct == stop_codon_2:
                     remove = False
-            if remove:
+            if remove:  # remove any StORF with disallowed stop codons
                 del storf_list[index]
     return storf_list
 
-def ip_set_constraints(prob: LpProblem, storf_groups: list, ip_vars: list) -> None:
-    # create constraints for each storf
-    ip_var_id = 0  # track the current IP variable (StORF) being used
-    
-    for group_id, storf_group in enumerate(storf_groups):
-        
-        if FILTER_ARGS.storf_range:
-            ip_set_storf_range(prob, storf_group, ip_vars, ip_var_id)
-        
-        if FILTER_ARGS.overlap_range:
-            ip_set_overlap_range(prob, storf_group, ip_vars, ip_var_id, group_id)  
-        else:
-            ip_set_no_overlaps(prob, storf_group, ip_vars, ip_var_id, group_id)
-
-        ip_var_id += len(storf_group)
-    
 
 def ip_filter(uf_value_storfs: list) -> list:
     '''
     Filter StORFs using integer programming
-    The IP model is informally defined as follows:
-    Maximise Sum(all identified StORF values)
-    S.t:  # !!!TODO!!! add each potential model in documentation
-        (1)
-        (2)
-    with bounds:
-        (a)
+    to maximise the number of StORFs selected
+    from the initial list, given the 
+    user applied constraints.
 
     return: a list of filtered StORFs
     '''
     # convert to list for lookup of selected storfs from IP solution
     storf_dict = {i: uf_value_storfs[i] for i in range(0, len(uf_value_storfs))}
     # initialise IP instance
-    prob = LpProblem("StORF_Knapsack_Problem", LpMaximize)
+    prob = LpProblem("StORF_IP_Filter", LpMaximize)
     # initialise IP variables with bounds
 
     storf_ids = [f"x_{i}" for i in range(0, len(uf_value_storfs))]
@@ -177,6 +208,7 @@ def ip_filter(uf_value_storfs: list) -> list:
     # solve the IP model
     prob.solve()
 
+    print(prob)
     # get selected StORFs from IP solution
     selected = []
     for var in prob.variables():
@@ -187,73 +219,13 @@ def ip_filter(uf_value_storfs: list) -> list:
 
 def read_fasta() -> list:
     unfiltered_storfs = []
-    with open("../../testout/smallstorftest.fasta") as storf_file:
+    with open(FILTER_ARGS.input_file) as storf_file:
         for line in storf_file:
             if line[0] == ">":
                 unfiltered_storfs.append([line, next(storf_file)])
-    #with open("../../testout/E-coli_output_no_filt.fasta") as storf_file:
-    #    for line in storf_file:
-    #        if line[0] == ">":
-    #            unfiltered_storfs.append([line, next(storf_file)])
-    return unfiltered_storfs
-
-
-def get_gc_average(storf_list: list) -> float: 
-    average_type = FILTER_ARGS.gc_profile[1]  # 0=mean, 1=median, 2=mode
-    if average_type == 0: # mean
-        total_gc = 0
-        num_storfs = len(storf_list)
-        for storf in storf_list:
-            total_gc += re.findall('[GC]', storf[1])
-        return total_gc/num_storfs
-    elif average_type == 1: # median
-        # order StORFs by length
-        ord_storfs = sorted(storf_list, key=lambda x: len(re.findall('[GC]', x[1])))
-        mid = (len(ord_storfs) - 1) // 2
-        if len(ord_storfs) % 2:  # odd
-            mid_gc_value = len(re.findall('[GC]', ord_storfs[mid][1]))
-            return mid_gc_value
-        else:  # even
-            # return interpolated median, average of both middle values
-            mid_1_gc = len(re.findall('[GC]', ord_storfs[mid][1]))
-            mid_2_gc = len(re.findall('[GC]', ord_storfs[mid + 1][1]))
-            mid_gc_value = (mid_1_gc + mid_2_gc) / 2.0
-            return mid_gc_value
-    elif average_type == 2:  # mode
-        # get gc count of each StORF
-        s_gc_len = [len(re.findall('[GC]', s[1])) for s in storf_list]
-        # get the most occuring gc count
-        mode_largest = max(set(s_gc_len), key=s_gc_len.count)
-
-        # TODO currently only selects the largest mode if multiple exist
-        # add some functionality for determining which mode to take?
-        return mode_largest
-
-
-def get_values(unfiltered_storfs: list):
-    """
-    Define values for each StORF according to set run-time parameters. 
-    These values will be added to the coefficients of each StORF variable 
-    in the maximisation objective function of the integer program.
-    """
-    # TODO rewrite lenght attribute already exists with the storf meta data
-    largest_storf = max(unfiltered_storfs, key=lambda x: len(x[1]))
-    ls_len = len(largest_storf[1])  # largest storf length
-
-    # define value of each StORF
-    for storf in unfiltered_storfs:
-        value = 1
-
-        if FILTER_ARGS.storf_len:
-            value += int(len(storf[1]))/ls_len
-
-        if FILTER_ARGS.simple_gc:
-            # GC-content of each StORF
-            value += len(re.findall('[GC]', storf[1])) / len(storf[1])
-
-        storf.append(value)
-
-    
+    # TMP test files
+    # "../../testout/smallstorftest.fasta"
+    # "../../testout/E-coli_output_no_filt.fasta"
     return unfiltered_storfs
 
 
@@ -267,52 +239,48 @@ def init_cl_args() -> argparse.ArgumentParser:
     """
     Run-time parameters dictate the type(s) of filtering to take place
     """
-    # TODO add argument exceptions
+    # TODO add argument exceptions/general error handling
     args = argparse.ArgumentParser(description='StORF filter options')
+    args.add_argument('-f', '--input_file', dest="input_file", type=str, 
+                      help="input fasta file destination")
     args.add_argument('-olr', '--overlap_range', dest='overlap_range', 
                       nargs=2, metavar=('MIN', 'MAX'), type=int,
                       help="the min and max StORF overlap range (nt)"
                      )
-    # default ~ 30, 100000
     args.add_argument('-sr' ,'--storf_range', dest='storf_range', nargs=2,
                       metavar=("MIN","MAX"), type=int,
                       help="(default: 30, 100000) the min and max StORF size (nt)"
                      )
     args.add_argument('-gc', '--gc_profile', dest='gc_profile', nargs=2,
                       type=int, metavar=("VAR","TYPE"),
-                      help='VAR: (default: 10) acceptable percentage range ' \
+                      help='VAR: acceptable percentage range ' \
                       'TYPE: (default: 0) 0=mean, 1=median, 2=mode. Filter ' \
                       'favouring StORFs closest to average gc content'
-                     )
-    args.add_argument('-sgc' ,'--simple_gc', dest='simple_gc', action='store_true',
-                      help='filter favouring highest GC content StORFs when choosing between overlaps'
                      )
     args.add_argument('-sct', '--stop_codon_type', dest='stop_codon_type', nargs='+',
                       choices=('TAG', 'TAA', 'TGA'), help='filter StORFs delimited by ' \
                       'one or more of the selected stop codons'
                      ) 
+    # TODO e.g. with average=mode, only select StORFs with least used stop codons
     args.add_argument('-asct', '--average_stop_codon', dest='stop_codon_av',
                       metavar=("TYPE"), type=int, 
                       help=" TYPE: (default: 0) 0=mean, 1=median, 2=mode"
                      )
-    args.add_argument('-slen', '--storf_length', dest="storf_len", action='store_true',
-                     help="favour selection of largest StORF in overlapping StORF groups"
-                     )
-
 
     global FILTER_ARGS
     FILTER_ARGS = args.parse_args()
-    if not (FILTER_ARGS.overlap_range or FILTER_ARGS.storf_range or FILTER_ARGS.simple_gc
-            or FILTER_ARGS.gc_profile or FILTER_ARGS.stop_codon_type or FILTER_ARGS.stop_codon_av
-            or FILTER_ARGS.storf_len):
+    if not FILTER_ARGS.input_file:
+        args.error("No fasta input StORF file provided (-f)")
+    if not (FILTER_ARGS.overlap_range or FILTER_ARGS.storf_range or FILTER_ARGS.gc_profile 
+            or FILTER_ARGS.stop_codon_type or FILTER_ARGS.stop_codon_av):
         args.error("No filtering options provided")
 
 
 def main():
+    # TODO general refactor+documentation
     init_cl_args()
     uf_storfs = read_fasta()
-    uf_value_storfs = get_values(uf_storfs)  # unfiltered StORFs with heuristic values
-    ip_filtered_storfs = ip_filter(uf_value_storfs)
+    ip_filtered_storfs = ip_filter(uf_storfs)
     fully_filtered_storfs = non_ip_filter(ip_filtered_storfs)
     write_fasta(fully_filtered_storfs)
 
