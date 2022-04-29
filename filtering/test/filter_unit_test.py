1b949f0986ee2b9b42da23370cb85b3ed271ee6d
oisin <oisinbrady@pm.me>
2022-04-29 17:29:33 +0100

    metric output refresh, refactoring, unittest skeleton

---
+++ filtering/test/filter_unit_test.py
@@ -0,0 +1,35 @@
+import unittest
+from ../storf_filter.py import filter_by_overlap, filter_by_size_range, filter_by_gc_range, filter_by_stop_codons, get_con_groups
+
+class TestOverlapFilter(unittest.TestCase):
+    # TODO
+    # e.g. self.assertEqual('foo'.upper(), 'FOO')
+    return
+
+
+class TestGCFilter(unittest.TestCase):
+    # TODO
+    # e.g. self.assertEqual('foo'.upper(), 'FOO')
+    return
+
+
+class TestSizeFilter(unittest.TestCase):
+    # TODO
+    # e.g. self.assertEqual('foo'.upper(), 'FOO')
+    return
+
+
+class TestCodonFilter(unittest.TestCase):
+    # TODO
+    # e.g. self.assertEqual('foo'.upper(), 'FOO')
+    return
+
+
+class TestContigGetter(unittest.TestCase):
+    # TODO
+    # e.g. self.assertEqual('foo'.upper(), 'FOO')
+    return
+    
+
+if __name__ == '__main__':
+    unittest.main()
